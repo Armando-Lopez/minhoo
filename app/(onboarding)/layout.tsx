0374a3e3e919b4e1492944605f210b8a3bb53fb9
Diego López <d.lopez@homecapital.com.co>
2024-06-05 14:33:42 -0500

    Revert "add header shadow scroll"
    This reverts commit 2e51c8c5a1e55dc6b9abbb20b0cce2ef7344c959.

--- app/(onboarding)/layout.tsx
+++ app/(onboarding)/layout.tsx
@@ -1,45 +1,22 @@
-"use client";
-
 import Footer from "@/components/layout/Footer";
 import OnboardingNav from "@/components/layout/OnboardingNav";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function OnboardingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    setIsScrolled(document.documentElement.scrollTop > 50);
-    function setHeaderScrollStyle() {
-      setIsScrolled(document.documentElement.scrollTop > 50);
-    }
-    document.addEventListener("scroll", setHeaderScrollStyle);
-
-    return () => document.removeEventListener("scroll", setHeaderScrollStyle);
-  }, []);
   return (
     <div className="bg-gray-150 min-h-svh">
-      <div className="min-h-svh flex flex-col justify-between">
-        <header
-          className={`md:sticky z-10 top-0 bg-gray-150 ${
-            isScrolled ? `md:shadow-md` : ""
-          }`}
-        >
-          <div className="container mx-auto flex justify-between py-3">
-            <a href="/" className="mt-[19px]">
-              <img
-                src="/minhoo-horizontal-logo.png"
-                alt="minhoo horizontal logo"
-                className="w-32"
-              />
-            </a>
-            <OnboardingNav />
-          </div>
+      <div className="container min-h-svh mx-auto flex flex-col justify-between">
+        <header className="md:sticky z-10 top-0 py-3 bg-gray-150 flex justify-between">
+          <a href="/" className="mt-[19px]">
+            <img src="/minhoo-horizontal-logo.png" alt="minhoo horizontal logo" className="w-32" />
+          </a>
+          <OnboardingNav />
         </header>
-        <main className="flex-grow container">{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </div>
     </div>
