3ea7e8186a0dbed2ab473a7a388092dda6dea946
Diego López <d.lopez@homecapital.com.co>
2024-06-10 13:18:17 -0500

    update render

--- app/web-vitals.tsx
+++ app/web-vitals.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { useReportWebVitals } from "next/web-vitals";
-import { useState } from "react";
+import { useRef } from "react";
 
 export function WebVitals() {
-  const [metrics, setMetrics] = useState({})
+  const metrics = useRef<any[]>([]);
+
   useReportWebVitals((metric) => {
-    setMetrics(metric)
+    metrics.current.push(metric);
+
+    // setMetrics(metric)
   });
+  console.log(metrics);
   return (
     <div className="fixed z-50 w-80 h-52 bottom-4 right-4 bg-black-1/50 rounded-lg shadow">
-      <div></div>
+      <div>{/* {metrics.current.value} */}</div>
     </div>
   );
 }
