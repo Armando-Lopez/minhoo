0f453e7b0f5f11636a84a3cfd13f9abc850a42b5
Armando-Lopez <lopexdiego5@gmail.com>
2024-08-31 17:34:27 -0500

    change text

--- app/(authenticated)/(chat)/my-shifts/page.tsx
+++ app/(authenticated)/(chat)/my-shifts/page.tsx
@@ -2,10 +2,10 @@ export default function myShiftsPage() {
   return (
     <>
       <div className="col-span-6 row-span-12 border-l border-l-gray-1/50">
-        shifts
+        Notifications
       </div>
       <div className="col-span-6 row-span-11">
-        notifications
+        shifts
       </div>
     </>  
   );
