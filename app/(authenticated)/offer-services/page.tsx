a3f5715fad09207453fc4c15b0ad609f70e4c8cd
Wildelmy <wildelmycolina@gmail.com>
2024-09-03 16:40:56 -0500

    c

--- app/(authenticated)/offer-services/page.tsx
+++ app/(authenticated)/offer-services/page.tsx
@@ -11,6 +11,15 @@ import { RadioGroup, RadioGroupItem } from "@/components/shared/ui/radio-group";
 import { Notifications } from "@/modules/notifications/components/Notifications";
 import React from "react";
 import { Label } from "@/components/shared/ui/label";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/shared/ui/accordion";
+import { OngoingServicesServiceCard } from "@/modules/request-services/ongoing-services/components/OngoingServicesServiceCard";
+import AppModal from "@/components/shared/AppModal";
+import { ShiftsDescription } from "@/modules/chats/shifts/ShiftsDescription";
 
 export default function OfferServicesPage() {
   const categories = [
@@ -62,41 +71,74 @@ export default function OfferServicesPage() {
     <>
       <div className="grid grid-cols-12 col-span-9 grid-rows-12 min-h-dvh">
         <div className="col-span-7 row-span-1 border-b border-gray-2 pt-9 pb-2 px-8 sticky top-0 bg-white">
-          <div className="flex items-center justify-between pb-3">
+          <div className="flex items-center justify-between pb-3 overflow-auto">
             <h2 className="text-lg font-medium">Offers</h2>
             <Popover>
               <PopoverTrigger asChild>
-                <Button className="rounded-full" variant="black">
+                <Button className="rounded-ful" variant="black">
                   <p className="text-gray-3">Filter by: </p>{" "}
                   <span className="text-white pl-1.5">All</span>
                   <AppIcon icon="arrow-down" className="ml-1 text-white" />
                 </Button>
               </PopoverTrigger>
-              <PopoverContent
-                align="end"
-                className="rounded-2xl p-5  space-y-5"
-              >
+              <PopoverContent align="end" className="rounded-2xl">
                 <>
-                  
                   <p className="mb-4 text-gray-3">Filter by</p>
-                  {categories.map((category, i) => (
-                    <RadioGroup key={i}>
-                      <Label
-                        htmlFor="per-hour"
-                        className="flex justify-between items-center font-normal cursor-pointer"
-                      >
-                        <p>
-                          {category.name} <span>{category.request}</span>
-                        </p>
+                  <Accordion type="multiple">
+                    <AccordionItem
+                      value="item-1"
+                      className="rounded-lg border-0"
+                    >
+                      <AccordionTrigger className="text-base text-center font-normal hover:no-underline gap-2">
+                        <div className="flex items-center gap-2">
+                          <AppIcon
+                            icon="location"
+                            className="text-primary-1"
+                            width={24}
+                          />
+                          <p>Countries</p>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent className="px-4">
+                        <p>Australia</p>
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem
+                      value="item-2"
+                      className="rounded-lg border-0"
+                    >
+                      <AccordionTrigger className="text-base text-center font-normal hover:no-underline gap-2 py-5">
+                        <div className="flex items-center gap-2">
+                          <AppIcon
+                            icon="gears"
+                            className="text-primary-1"
+                            width="24"
+                          />
+                          <p>Categories</p>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent className="p-0 space-y-5 pb-10">
+                        {categories.map((category, i) => (
+                          <RadioGroup key={i}>
+                            <Label
+                              htmlFor="per-hour"
+                              className="flex justify-between items-center font-normal cursor-pointer"
+                            >
+                              <p>
+                                {category.name} <span>{category.request}</span>
+                              </p>
 
-                        <RadioGroupItem
-                          id="per-hour"
-                          value="per-hour"
-                          className="w-4 h-4"
-                        />
-                      </Label>
-                    </RadioGroup>
-                  ))}
+                              <RadioGroupItem
+                                id="per-hour"
+                                value="per-hour"
+                                className="w-4 h-4"
+                              />
+                            </Label>
+                          </RadioGroup>
+                        ))}
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
                 </>
               </PopoverContent>
             </Popover>
@@ -113,7 +155,39 @@ export default function OfferServicesPage() {
         <div className="col-span-5 row-span-12 border-l border-gray-2">
           <Notifications />
         </div>
-        <div className="col-span-7 row-span-11 border-l border-gray-2"></div>
+        <div className="col-span-7 row-span-11 border-l border-gray-2">
+          <div className="flex flex-col items-center mt-5">
+          <AppModal
+          activator={
+            <div className="w-[358px] mx-auto mt-5">
+            <OngoingServicesServiceCard
+              icon="building"
+              category="On-site"
+              description="I urgently need to fix a rupture in one of the pipes in my apartment."
+              jobType="Handyman"
+              price="150.00"
+              time="12 hours ago"
+              location="Brisbane, Australia"
+            />
+          </div>
+          }
+          className="rounded-2xl h-fit ml-auto hide-scroll-bar"
+        >
+          <ShiftsDescription />
+        </AppModal>
+            <div className="w-[358px]">
+              <OngoingServicesServiceCard
+                icon="building"
+                category="On-site"
+                description="I urgently need to fix a rupture in one of the pipes in my apartment."
+                jobType="Handyman"
+                price="150.00"
+                time="12 hours ago"
+                location="Brisbane, Australia"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
